e4e7b6ff224aab41b5bcb33664dbd2612cb74ddd
Reza ADB <adbrezaooo@gmail.com>
2020-06-29 23:53:03 +0430

    get os info Done!

---
+++ Phase 1/code/GetData.bat
@@ -0,0 +1,5 @@
+$osInfo = Get-ComputerInfo -property OsType, OsVersion, OsName, OsLocalDateTime
+"OS Type: " + $osInfo.OsType | out-file 'datas\osInfo.txt'
+"OS Version: " + $osInfo.OsVersion | Add-Content  'datas\osInfo.txt'
+"OS Name: " + $osInfo.OsName | Add-Content 'datas\osInfo.txt'
+"OS LocalDateTime: " + $osInfo.OsLocalDateTime | Add-Content 'datas\osInfo.txt'
