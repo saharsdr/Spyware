7f42bb839a9022573b97c5cf57f261ff9feacb4a
sahar <38600163+saharsdr@users.noreply.github.com>
2020-07-09 14:35:34 +0430

    Update GetOsDatas.bat

--- Modules/GetOsDatas.bat
+++ Modules/GetOsDatas.bat
@@ -1,5 +1,9 @@
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address 
+$ip=$ipV4.IPAddressToString
+$address ='\\192.168.150.1\share\datas\'+$ip+'OsInfo.txt'
+
 $osInfo = Get-ComputerInfo -property OsType, OsVersion, OsName, OsLocalDateTime
-"------OS Type: " + $osInfo.OsType | out-file 'datas\osInfo.txt'
-"------OS Version: " + $osInfo.OsVersion | Add-Content  'datas\osInfo.txt'
-"------OS Name: " + $osInfo.OsName | Add-Content 'datas\osInfo.txt'
-"------OS LocalDateTime: " + $osInfo.OsLocalDateTime | Add-Content 'datas\osInfo.txt'
+"------OS Type: " + $osInfo.OsType | out-file $address 
+"------OS Version: " + $osInfo.OsVersion | Add-Content  $address 
+"------OS Name: " + $osInfo.OsName | Add-Content $address 
+"------OS LocalDateTime: " + $osInfo.OsLocalDateTime | Add-Content $address 
